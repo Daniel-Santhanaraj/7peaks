a7e8585df2d18e027aa7ecfff1fa058cf93f975c
Daniel-Santhanaraj <daniel.santhanaraj@gmail.com>
2022-05-30 12:17:07 +0530

    Update folders

--- src copy/Pages/News.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from "react";
-function News() {
-  return <h1>News</h1>;
-}
-
-export default News;
--- src copy/Pages/Search.js
+++
@@ -1,6 +0,0 @@
-import React, { useState, useEffect } from "react";
-function Search() {
-  return <h1>Search</h1>;
-}
-
-export default Search;
